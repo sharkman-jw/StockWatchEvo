131785c3f0686b43b608cf7b9f757dc35e251902
Sharkman <sharkman.jw@gmail.com>
2011-12-17 01:14:53 -0500

    created SObject, improved runtime attribute and handlers binding

--- js/front/popup.js
+++ js/front/popup.js
@@ -22,19 +22,23 @@ debugUtils.warn = function(s) {
 };
 
 function track(s) { debugUtils.show(s); }
+function trackObj(obj) { debugUtils.show(JSON.stringify(obj)); }
 
 
 
-
+/**
+ * Class Popup
+ */
 function Popup() {
-  SView.apply(this, ["popup"]);
+  SBag.apply(this, ["sb_popup"]);
 };
-Popup.prototype = new SView();
+Popup.prototype = new SBag();
 Popup.prototype.constructor = Popup;
 
 Popup.prototype.init = function() {
   this.initLocalData();
   this.initUI();
+  this.initQuoters();
 };
 
 Popup.prototype.initLocalData = function() {
@@ -49,10 +53,30 @@ Popup.prototype.initLocalData = function() {
   
 };
 
+Popup.prototype.initQuoters = function() {
+  this.gQuoter = new GoogleQuoter();
+  var sdm = this.sdm;
+  /**
+   * Callback for GoogleQuoter after getting quote from providers
+   * and now process the StockData object.
+   */
+  this.gQuoter.resultStockDataHandler = function(stockData) {
+    // This incoming stockData contains all data except bids and asks,
+    // so we just use everything except those two.
+    var sd = sdm.getStockData(stockData.keyTicker);
+    if (sd) {
+      stockData.bids = sd.bids;
+      stockData.asks = sd.asks;
+    }
+    sdm.add(stockData);
+    track(stockData + ": " + stockData.last);
+  };
+};
+
 Popup.prototype.initUI = function() {
   this.topArea = new TopArea();
   this.topArea.init();
-  this.addSubview(this.topArea);
+  this.add(this.topArea);
   // TODO
 };
 
@@ -61,24 +85,44 @@ Popup.prototype.handleInitFail = function() {
 };
 
 Popup.prototype.run = function() {
+  this.loopWork();
 };
 
 /**
  * Load stock data from local storage.
  */
 Popup.prototype.loadStockData = function() {
+  var sd = null;
+  var symbols = this.subs.getSymbols();
+  var n = symbols.length;
+  for (var i = 0; i < n; ++ i) {
+    sd = StockData.retrieve(symbols[i]);
+    if (sd)
+      this.sdm.add(sd);
+    else
+      track("Couldn't load stock data: " + symbols[i]);
+  }
 };
 
-Popup.prototype.refreshUI = function() {
-};
+/**
+ * Render popup view.
+ */
+/*Popup.prototype.render = function() {
+  // TODO
+  SBag.prototype.render.call(this);
+};*/
 
 Popup.prototype.loopWork = function() {
   this.loadStockData();
-  this.refreshUI();
+  this.topArea.setStockData(this.sdm.getStockData("NYSE:RENN"));
+  this.render();
   // scehdule next round
   var setting = settingsManager.getSetting('data_refresh_interval');
   var timeout = 1000 * (setting ? setting.getValue() : 10);
   //window.setTimeout(loopRefreshQuotes, timeout);
 };
 
+Popup.prototype.quote = function(input) {
+  this.gQuoter.requestGroupQuotes([input]);
+};
 
--- js/front/top_area.js
+++ js/front/top_area.js
@@ -2,39 +2,99 @@
 
 /**
  * @fileoverview Top area
- *               - symbol input
- *               - 
  * @author sharkman.jw@gmail.com (Joshua Wang)
  */
-
-function SymbolInput(selector) {
-  SInput.apply(this, ["text", selector, null]);
+function DetailPanel() {
+  SBag.apply(this, ["sb_detail_panel"]);
+  this.stockData = null;
 };
-SymbolInput.prototype = new SInput();
-SymbolInput.prototype.constructor = SymbolInput;
+DetailPanel.prototype = new SBag();
+DetailPanel.prototype.constructor = DetailPanel;
 
-SymbolInput.prototype.handleEvent = function(event) {
-  if (event.type == "keydown") {
-    // handle ENTER, ARROW UP/DOWN
-    track("here");
-    return true;
-  } else if (event.type == "keyup") {
-    // validate input, and request symbol hints
-    return true;
-  }
-  return false;
+DetailPanel.prototype.init = function() {
+  var sn = new SNode("detail_prc", "#detail_prc", {"dp": this});
+  sn.bind("renderSelf", function() {
+    var sd = this.attr("dp").stockData;
+    this.jqo.text(sd ? sd.last : "");
+  });
+  this.add(sn);
+  
+  sn = new SNode("detail_status", "#detail_status", {"dp": this});
+  sn.bind("renderSelf", function() {
+    var sd = this.attr("dp").stockData;
+    this.jqo.text(sd ? sd.tradingStatus : "");
+  });
+  this.add(sn);
+  
+  sn = new SNode("detail_c", "#detail_c", {"dp": this});
+  sn.bind("renderSelf", function() {
+    var sd = this.attr("dp").stockData;
+    this.jqo.text(sd ? sd.change : "");
+  });
+  this.add(sn);
+  
+  sn = new SNode("detail_cp", "#detail_cp", {"dp": this});
+  sn.bind("renderSelf", function() {
+    var sd = this.attr("dp").stockData;
+    this.jqo.text(sd ? sd.percentChange : "");
+  });
+  this.add(sn);
 };
 
+DetailPanel.prototype.setStockData = function(sd) {
+  if (!sd)
+    return;
+  this.stockData = sd;
+};
+
+
+ 
 
 function TopArea() {
-  SView.apply(this, ["top_area"]);
+  SBag.apply(this, ["sb_top_area"]);
+  
+  this.headStockData = null;
 };
-TopArea.prototype = new SView();
+TopArea.prototype = new SBag();
 TopArea.prototype.constructor = TopArea;
 
 TopArea.prototype.init = function() {
-  var si = new SymbolInput("#symbol_input");
-  this.addNode(si, "symbol_input");
+  this.add(this.createSymbolInput());
+  var dp = new DetailPanel();
+  dp.init();
+  this.add(dp);
+};
+
+TopArea.prototype.createSymbolInput = function() {
+  var si = new SNode("symbol_input", "#symbol_input");
+  si.bind("onkeydown", function(event) {
+    // handle ENTER, ARROW UP/DOWN
+    //this.attr("typing", true);
+    if (event.keyCode == 13) {
+      var input = "";
+      input = this.jqo.val().toUpperCase();
+      jQuery.trim(input);
+      if (input) {
+        try {
+          track("Request: " + input);
+          popup.quote(input);
+        } catch (err) {
+          track("Fetch quote failed: " + input);
+        }
+      }
+    }
+    //track("process " + event.type + "@" + this.name);
+  });
+  si.bind("onkeyup", function(event) {
+    // handle ENTER, ARROW UP/DOWN
+    //track("process " + event.type + "@" + this.name);
+  });
+  return si;
+};
+
+TopArea.prototype.setStockData = function(sd) {
+  this.headStockData = sd;
+  this.get("sb_detail_panel").setStockData(sd);
 };
 
 
